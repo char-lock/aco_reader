7361ef4a27a3dbe78c6c39883dc6d4bbef3eb29b
C. Lockett <103963818+char-lock@users.noreply.github.com>
2022-10-28 18:00:39 -0400

    Initial commit

---
+++ aco_reader/main.py
@@ -0,0 +1,145 @@
+""" This is the entry point to using aco_reader. It only really
+contains metadata.
+
+"""
+#
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# For more information, please refer to <https://unlicense.org>
+#
+import os
+import sys
+from typing import Dict, List, Optional, Union
+from reader import read_file
+
+__version__ = '0.1.0'
+
+
+# Argument variables
+ARGUMENTS: Dict[str, Union[str, bool]] = {
+    'output_directory': '.' + os.sep,
+    'input_directory': '.' + os.sep,
+    'fail_quiet': False,
+    'verbose': False,
+    'text_only': False
+}
+
+
+def vprint(*values: object, sep: Optional[str] = None, end: Optional[str] = None):
+    """ Prints a message to the console only if the verbose flag has
+    been toggled on.
+
+    """
+    if (ARGUMENTS['verbose']):
+        print(values, sep=sep, end=end)
+
+def print_preamble() -> None:
+    """ Prints the application's name and copyright information
+    to the console.
+
+    """
+    print('ACO Reader v0.1.0 Color Swatch Export Tool')
+    print('Copyright (c) 2022, C. Lockett\n')
+    print('This software is provided "as is", without warranty of any kind,')
+    print('express or implied. For more details, see the LICENSE file.\n')
+
+def print_help() -> None:
+    """ Prints the application help screen to the console. """
+    print('Optional Arguments:')
+    print('    -h, --help                 Show help')
+    print('    -o, --out-dir=<dir>        Desired directory for output files.')
+    print('    -i, --in-dir=<dir>         Directory containing target .aco files.')
+    print('    -q, --quiet                Fail quietly and continue onward with processing.')
+    print('    -v, --verbose              Display non-critical messages.')
+    print('    -t, --text                 Export text files only.')
+
+if __name__ == '__main__':
+    print_preamble()
+
+    # Check the passed arguments to ensure that there's not any.
+    # If there's an argument, we'll process it accordingly.
+    if (len(sys.argv) > 1):
+        # The first check we'll do is if we find the help argument.
+        # If we do, then that's the one and only thing to do.
+        if ('-h' in sys.argv or '--help' in sys.argv):
+            print_help()
+            exit()
+        for i, arg in enumerate(sys.argv[1:]):
+            # -o, --out-dir=<dir>
+            if (arg == '-o' or '--out-dir=' in arg):
+                # Retrieve the target output directory from the argument.
+                t_out_dir: str = ''
+                if arg == '-o':
+                    t_out_dir = sys.argv[i + 2]
+                    i += 1
+                else:
+                    t_out_dir = arg.replace('--out-dir=', '')
+                t_out_dir = os.path.abspath(t_out_dir)
+                if t_out_dir[-1] != os.sep: t_out_dir += os.sep
+                # Check that the target output directory exists.
+                if (os.path.exists(t_out_dir)):
+                    ARGUMENTS['output_directory'] = t_out_dir
+                else:
+                    print(f'aco_reader: unable to find specified output directory. ({t_out_dir})')
+                    sys.exit(0)
+            # -i, --in-dir=<dir>
+            if (arg == '-i' or '--in-dir=' in arg):
+                # Retrieve the target output directory from the argument.
+                t_in_dir: str = ''
+                if arg == '-i':
+                    t_in_dir = sys.argv[i + 2]
+                    i += 1
+                else:
+                    t_in_dir = arg.replace('--in-dir=', '')
+                t_in_dir = os.path.abspath(t_in_dir)
+                if t_in_dir[-1] != os.sep: t_in_dir += os.sep
+                # Check that the target output directory exists.
+                if (os.path.exists(t_in_dir)):
+                    ARGUMENTS['input_directory'] = t_in_dir
+                else:
+                    print(f'aco_reader: unable to find specified input directory. ({t_in_dir})')
+                    sys.exit(0)
+            # -q, --quiet
+            if (arg == '-q' or arg == '--quiet'):
+                ARGUMENTS['fail_quiet'] = True
+            # -v, --verbose
+            if (arg == '-v' or arg == '--verbose'):
+                ARGUMENTS['verbose'] = True
+            # -(t, --text
+            if (arg == '-t' or arg == '--t'):
+                ARGUMENTS['text_only'] = True
+    # Get a list of aco files in the input directory.
+    files: list[str] = [
+        f for f in os.listdir(ARGUMENTS['input_directory'])
+            if os.path.isfile(ARGUMENTS['input_directory'] + f)
+                and f[-3:] == 'aco'
+    ]
+    if (len(files) > 0):
+        vprint(f'Found {len(files)} .aco files to process in input directory.')
+    else:
+        print('aco_reader: unable to find any .aco files in the input directory.')
+        sys.exit(0)
+    for _f in files:
+        read_file(_f, xargs=ARGUMENTS)
+ 
---
+++ aco_reader/reader.py
@@ -0,0 +1,338 @@
+""" Handles reading aco files and interpreting the color swatch data
+encoded inside.
+
+"""
+#
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# For more information, please refer to <https://unlicense.org>
+#
+from io import BytesIO
+import os
+from typing import Optional
+
+
+def read_int16(file: BytesIO) -> int:
+    """ Reads an unsigned 16-bit integer from `file` and returns it as
+    an int.
+
+
+    #### Parameters
+    * file: BytesIO
+      * input stream from a file from which to read
+
+
+    #### Returns
+    * int
+      * the 16-bit integer as an int
+
+    """
+    # Read two bytes from the file; .aco files are in big-endian order,
+    # so we don't need to flip anything.
+    _c: bytes = file.read(2)
+    # Convert the read bytes straight to an integer.
+    _v: int = int(_c.hex(), base=16)
+    # We'll perform a quick check as we return for a negative value.
+    # This lets us treat the value as unsigned properly.
+    return _v if _v >= 0 else (_v + (1 << 16))
+
+
+def read_int32(file: BytesIO) -> int:
+    """ Reads an unsigned 32-bit integer from `file` and returns it as
+    an int.
+
+
+    #### Parameters
+
+    * file: BytesIO
+        * input stream from a file from which to read
+
+
+    #### Returns
+
+    * int
+      * the 32-bit integer as an int
+
+    """
+    # Read four bytes from the file; .aco files are in big-endian order,
+    # so we don't need to flip anything.
+    _c: bytes = file.read(4)
+    # Convert the read bytes straight to an integer.
+    _v: int = int(_c.hex(), base=16)
+    # We'll perform a quick check as we return for a negative value.
+    # This lets us treat the value as unsigned properly.
+    return _v if _v >= 0 else (_v + (1 << 32))
+
+
+def read_string(file: BytesIO, length: int) -> str:
+    """ Reads a string from `file` of size `length` and returns it.
+
+
+    #### Parameters
+
+    * file: BytesIO
+      * input stream from a file from which to read
+
+    * length: int
+      * length of string in bytes
+
+
+    #### Returns
+
+    * str
+      * the next string of `length` length from `file`
+
+    """
+    _r: str = ''
+    for _ in range(length):
+        _r += file.read(2).decode()
+    return _r.replace('\0', '')
+
+
+def get_rgb(values: list[int]) -> list[float]:
+    """ Returns a list of values for an RGB color definition.
+
+
+    #### Parameters
+
+    * values: list[int]
+      * a list containing a minimum of 3 values
+      * it is assumed that all values are retrieved via read_int16
+
+
+    #### Returns
+    * list[float]
+      * a list containing the converted values
+
+    """
+    if (len(values) < 3):
+        err: str = 'Too few values provided for RGB conversion.\n    '
+        err += f'Got {len(values)}, should be at least 3.'
+        raise ValueError(err)
+    return [(v / 256) for v in values[:3]]
+
+
+def get_hsb(values: list[int]) -> list[float]:
+    """ Returns a list of values for an HSB color definition.
+
+
+    #### Parameters
+
+    * values: list[int]
+      * a list containing a minimum of 3 values
+      * it is assumed that all values are retrieved via read_int16
+
+
+    #### Returns
+    * list[float]
+      * a list containing the converted values
+
+    """
+    if (len(values) < 3):
+        err: str = 'Too few values provided for HSB conversion.\n    '
+        err += f'Got {len(values)}, should be at least 3.'
+        raise ValueError(err)
+    return [values[0] / 182.04, values[1] / 655.35, values[2] / 655.35]
+
+
+def get_cmyk(values: list[int]) -> list[float]:
+    """ Returns a list of values for a CMYK color definition.
+
+
+    #### Parameters
+
+    * values: list[int]
+      * a list containing a minimum of 4 values
+      * it is assumed that all values are retrieved via read_int16
+
+
+    #### Returns
+    * list[float]
+      * a list containing the converted values
+
+    """
+    if (len(values) < 4):
+        err: str = 'Too few values provided for CMYK conversion.\n    '
+        err += f'Got {len(values)}, should be at least 4.'
+        raise ValueError(err)
+    return [
+        values[0] / 65535 * 100,
+        values[1] / 65535 * 100,
+        values[2] / 65535 * 100,
+        values[3] / 65535 * 100
+    ]
+
+
+def get_lab(values: list[int]) -> list[float]:
+    """ Returns a list of values for an LAB color definition.
+
+
+    #### Parameters
+
+    * values: list[int]
+      * a list containing a minimum of 3 values
+      * it is assumed that all values are retrieved via read_int16
+
+
+    #### Returns
+    * list[float]
+      * a list containing the converted values
+
+    """
+    if (len(values) < 3):
+        err: str = 'Too few values provided for LAB conversion.\n    '
+        err += f'Got {len(values)}, should be at least 3.'
+        raise ValueError(err)
+    return [
+        values[0] / 100,
+        (values[1] / 100) - 128 if -128 <= (values[1] / 100) - 128 <= 127 else ((values[1] - (1 << 16)) / 100),
+        (values[2] / 100) - 128 if -128 <= (values[2] / 100) - 128 <= 127 else ((values[2] - (1 << 16)) / 100)
+    ]
+
+
+def interpret_colors(color_space: int, values: list[int]) -> str:
+    """ Interprets the provided color values according to the rules of
+    the provided color space.
+
+
+    #### Parameters
+
+    * color_space: int
+      * an integer representing a specified color space.
+      * see https://www.adobe.com/devnet-apps/photoshop/fileformatashtml/#50577411_22664
+
+    * values: list[int]
+      * a list of values to be converted according to the color space.
+      * must be at least three values long
+
+
+    #### Returns
+      * str
+        * a string in the format prepended with a label for the color
+          space, followed by comma-separated converted values.
+
+    """
+    if (len(values) < 3):
+        err: str = 'Too few values provided to interpret color values.'
+        err += f'\n    Got {len(values)}, should be at least 3.'
+        raise ValueError(err)
+    _col: list[float] = []
+    _l: str = ''
+    if color_space == 0:
+        _l = 'RGB'
+        _col.extend(get_rgb(values))
+    elif color_space == 1:
+        _l = 'HSB'
+        _col.extend(get_hsb(values))
+    elif color_space == 2:
+        _l = 'CMYK'
+        _col.extend(get_cmyk(values))
+    elif color_space == 7:
+        _l = 'LAB'
+        _col.extend(get_lab(values))
+    else:
+        raise ValueError(f"Unhandled color space. color_space = {color_space}")
+    return _l + ', ' + ', '.join([f'{c:.2f}' for c in _col])
+
+
+def read_color_values(file: BytesIO) -> str:
+    """ Reads the color values from `file`, interprets them, and returns
+    a string that can be written to a file.
+
+
+    #### Parameters
+
+    * file: BytesIO
+      * input stream from a file from which to read the next color
+        values
+
+
+    #### Returns
+
+    * str
+      * a string to be written to a text file containing the interpreted
+        value for the next color value
+
+    """
+    color_space: int = read_int16(file)
+    c_val_0: int = read_int16(file)
+    c_val_1: int = read_int16(file)
+    c_val_2: int = read_int16(file)
+    c_val_3: int = read_int16(file)
+    n_len: int = read_int32(file)
+    s_name: str = read_string(file, n_len)
+    col: str = interpret_colors(color_space, [c_val_0, c_val_1, c_val_2, c_val_3])
+    return s_name + ", " + col + "\n"
+
+
+def read_file(filename: str, xargs: Optional[dict] = None) -> None:
+    """ Reads a the file indicated by `filename` and writes the
+    interpreted data to a text file.
+
+
+    #### Parameters
+
+    * filename: str
+      * the path to the target .aco file to read
+
+    * args: Optional[dict]
+      * a dictionary containing optional arguments
+
+    """
+    if (os.path.sep == xargs['input_directory'][-1]):
+        filename = xargs['input_directory'] + filename
+    else:
+        filename = xargs['input_directory'] + os.sep + filename
+    _f: BytesIO = open(filename, 'rb')
+    verbose: bool = xargs['verbose']
+    fail_quiet: bool = xargs['fail_quiet']
+    # Check the file version.
+    version: int = read_int16(_f)
+    if (version != 2):
+        if (not fail_quiet):
+            raise ValueError(f"This script is currently unable to process .aco files of any version other than 2.\nversion = {version}")
+        else:
+            return
+    # Get color count.
+    color_count: int = read_int16(_f)
+    if (verbose):
+        print(f"Reading {color_count} colors from '{filename}' ...")
+    # Process all of the colors.
+    if (fail_quiet):
+        fail_count: int = 0
+    for _ in range(color_count):
+        try:
+            _wr: str = read_color_values(_f)
+        except ValueError as ex:
+            if (fail_quiet):
+                fail_count += 1
+                continue
+            else:
+                raise ex
+        with open(filename[:-4].replace(xargs['input_directory'], xargs['output_directory']) + '.txt', 'a+') as f:
+            f.write(_wr)
+    if (verbose):
+        print(f"Finished reading '{filename}'. Results saved in '{filename[:-4]}.txt'.")
+        if (fail_quiet):
+            print(f'Failed to interpret {fail_count} swatches.')
